9785a03ae0accb69b4505df530cd8529f1866637
everypine <kenton.lim@gmail.com>
2026-05-20 20:59:11 +0900

    test(store): FakeStoreRepository 추가

---
+++ src/test/java/roomescape/domain/store/repository/FakeStoreRepository.java
@@ -0,0 +1,31 @@
+package roomescape.domain.store.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import roomescape.domain.store.entity.Store;
+
+public class FakeStoreRepository implements StoreRepository {
+
+    private final AtomicLong id = new AtomicLong(0);
+    private final List<Store> stores = new ArrayList<>();
+
+    public FakeStoreRepository() {
+        save(Store.create("강남점"));
+    }
+
+    public Store save(Store store) {
+        Store savedStore = Store.create(store.getName()).withId(id.addAndGet(1));
+        stores.add(savedStore);
+        return savedStore;
+    }
+
+    @Override
+    public Optional<Store> findById(Long id) {
+        return stores.stream()
+            .filter(store -> Objects.equals(store.getId(), id))
+            .findFirst();
+    }
+}
