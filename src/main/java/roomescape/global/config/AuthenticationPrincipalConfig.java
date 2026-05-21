33c83a6754422ec6017bee56e7163c81aeeee52d
everypine <kenton.lim@gmail.com>
2026-05-21 14:29:26 +0900

    fix(config): 매니저 API에서 매니저 권한을 검사하지 않던 문제 수정

--- src/main/java/roomescape/global/config/AuthenticationPrincipalConfig.java
+++ src/main/java/roomescape/global/config/AuthenticationPrincipalConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.global.auth.AdminAuthorizationInterceptor;
 import roomescape.global.auth.AuthenticationInterceptor;
+import roomescape.global.auth.ManagerAuthorizationInterceptor;
 import roomescape.global.auth.repository.MemberRepository;
 import roomescape.global.auth.resolver.AuthTokenArgumentResolver;
 import roomescape.global.auth.resolver.LoginMemberArgumentResolver;
@@ -16,13 +17,16 @@ public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
 
     private final AuthenticationInterceptor authenticationInterceptor;
     private final AdminAuthorizationInterceptor adminAuthorizationInterceptor;
+    private final ManagerAuthorizationInterceptor managerAuthorizationInterceptor;
     private final MemberRepository memberRepository;
 
     public AuthenticationPrincipalConfig(AuthenticationInterceptor authenticationInterceptor,
         AdminAuthorizationInterceptor adminAuthorizationInterceptor,
+        ManagerAuthorizationInterceptor managerAuthorizationInterceptor,
         MemberRepository memberRepository) {
         this.authenticationInterceptor = authenticationInterceptor;
         this.adminAuthorizationInterceptor = adminAuthorizationInterceptor;
+        this.managerAuthorizationInterceptor = managerAuthorizationInterceptor;
         this.memberRepository = memberRepository;
     }
 
@@ -33,7 +37,7 @@ public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
                 "/api/stores", "/api/stores/**");
         registry.addInterceptor(adminAuthorizationInterceptor)
             .addPathPatterns("/api/admin/**");
-        registry.addInterceptor(authenticationInterceptor)
+        registry.addInterceptor(managerAuthorizationInterceptor)
             .addPathPatterns("/api/manager/**");
     }
 
