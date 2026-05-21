7508e74d95bb4c724d4073fc19fbbfa3a7f87d37
everypine <kenton.lim@gmail.com>
2026-05-21 17:39:53 +0900

    fix(store): 사용자 컨트롤러가 `@Controller`로 등록되어 있던 문제 수정

--- src/main/java/roomescape/domain/store/controller/StoreController.java
+++ src/main/java/roomescape/domain/store/controller/StoreController.java
@@ -3,13 +3,13 @@ package roomescape.domain.store.controller;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.store.dto.response.StoreResponseDto;
 import roomescape.domain.store.service.StoreService;
 
-@Controller
+@RestController
 @RequestMapping("/api/stores")
 public class StoreController {
 
